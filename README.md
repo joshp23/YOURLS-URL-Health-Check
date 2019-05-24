91ab423304997b27dd4d90da715035f5694f5efa
Joshua P Panter <joshu@unfettered.net>
2019-05-24 07:37:34 -0400

    upload plugin | update readme

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # YOURLS-URL-Health-Check
 A YOURLS plugin that checks submitted URL's for validity, reachability, and redirection
+
+- If a submitted URL does not meet [RFC 2396](http://www.faqs.org/rfcs/rfc2396.html) standards, it is rejected
+- If a submitted URL is unreachable (times out after 3 seconds), it is rejected
+- If a URL is a redirect, the final destination is stored to avoid nested redirects
+
+### Requirements
+- A working [YOURLS](https://github.com/YOURLS/YOURLS) installation
+- php-curl installed and activated
+
+### INSTALLATION
+
+1. Place the url-health-check folder in YOURLS/user/plugins/
+2. Activate in the Admin interface
+
+### TODO
+
+- Make timeout limit adjustable
+- Check old links for reachability?
+
+### Support Dev
+All of my published code is developed and maintained in spare time, if you would like to support development of this, or any of my published code, I have set up a Liberpay account for just this purpose. Thank you.
+
+<noscript><a href="https://liberapay.com/joshu42/donate"><img alt="Donate using Liberapay" src="https://liberapay.com/assets/widgets/donate.svg"></a></noscript>
+
+===========================
+
+    Copyright (C) 2019 Josh Panter
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
---
+++ url-health-check/plugin.php
@@ -0,0 +1,73 @@
+<?php
+/*
+Plugin Name: URL Health Check
+Plugin URI: https://github.com/joshp23/YOURLS-URL-Health-Check
+Description: Checks submitted long URL's for validity, reachability, and redirection
+Version: 0.0.1
+Author: Josh Panter
+Author URI: https://unfettered.net
+*/
+// No direct call
+if( !defined( 'YOURLS_ABSPATH' ) ) die();
+yourls_add_filter('shunt_add_new_link', 'url_health_check');
+function url_health_check( $is, $url, $keyword = '' , $title = '') {
+
+	$return = false;
+
+	// validate url
+	if( !filter_var($url, FILTER_VALIDATE_URL) ) {
+		$return['status']    = 'fail';
+		$return['code']      = 'error:invalid';
+		$return['message']   = yourls__( 'HealthCheck: Invalid URL');
+		$return['statusCode'] = '400';
+	}
+
+	if ( $return == false && in_array( yourls_get_protocol( $url ), array( 'http://', 'https://' ) ) ) {
+
+		$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_HEADER, false);
+			curl_setopt($ch, CURLOPT_NOBODY, true);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT , 0); 
+			curl_setopt($ch, CURLOPT_TIMEOUT, 3); // TODO Is this reasonable?
+		$output = curl_exec($ch);
+		$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		$finalURL = curl_getinfo($ch,CURLINFO_EFFECTIVE_URL );
+			curl_close($ch);
+
+		// update url to redirection desstiantion if needed
+		if ( $finalURL !== $url ) {
+
+			define( 'URLHC_REDIRECT', $finalURL );
+			yourls_add_filter( 'add_new_link', 'URLHC_redirection' );
+
+		// otherwise, check for success
+		} elseif ( $httpcode !== 200 ) {
+			// and if not...
+			$return['status']   = 'fail';
+			$return['code']     = 'error:url';
+			$return['message']  = yourls__( 'The destination URL is unreachable.' );
+			$return['statusCode'] = 200; // regardless of result, this is still a valid request
+		}
+	}
+
+	return $return;
+}
+
+function URLHC_redirection( $return, $old , $keyword, $title  ) {
+	$new = URLHC_REDIRECT;
+	global $ydb;
+	$table = YOURLS_DB_TABLE_URL;
+	$update = null;
+	if (version_compare(YOURLS_VERSION, '1.7.3') >= 0) {
+		$binds = array('old' => $old, 'new' => $new);
+		$sql = "UPDATE `$table` SET `url` = REPLACE(`url`, :old, :new) WHERE `url` = :old";
+		$update = $ydb->fetchAffected($sql, $binds);
+	} else {
+		$update = $ydb->query("UPDATE `$table` SET `url` = REPLACE(`url`, '$old', '$new') WHERE `url` = '$old'");
+	}
+	$return['url']['url'] = $new;
+	return $return;
+}
